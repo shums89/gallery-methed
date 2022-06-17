97a5d27289807c7fc1f69f86c82058be2a812374
Александр <sh889@ya.ru>
2022-06-17 20:32:35 +0500

    Бесконечный скролл

--- scripts/renderGallery.js
+++ scripts/renderGallery.js
@@ -1,10 +1,13 @@
 import { createCardPhoto } from "./createCardPhoto.js";
 import { createElem } from "./createElem.js";
+import { scrollLoad } from "./scrollLoad.js";
 
 export const renderGallery = (wrapper, photos) => {
   const gallery = createElem('ul', {
     className: 'grid',
   });
+  const endElem = createElem('div');
+
   wrapper.append(gallery);
 
   const grid = new Masonry(gallery, {
@@ -19,5 +22,7 @@ export const renderGallery = (wrapper, photos) => {
   Promise.all(cards).then((cards) => {
     gallery.append(...cards);
     grid.appended(cards);
+    wrapper.append(endElem);
+    scrollLoad(gallery, grid, endElem);
   });
 }
---
+++ scripts/scrollLoad.js
@@ -0,0 +1,23 @@
+import { getData } from "./getData.js";
+import { createCardPhoto } from "./createCardPhoto.js";
+
+export const scrollLoad = (gallery, grid, endElement) => {
+  const observer = new IntersectionObserver(
+    async (entries) => {
+      if (entries[0].isIntersecting) {
+        const photos = await getData();
+        const cards = photos.map(createCardPhoto);
+
+        Promise.all(cards).then((cards) => {
+          gallery.append(...cards);
+          grid.appended(cards);
+        });
+      }
+    },
+    {
+      rootMargin: '150px',
+    }
+  );
+
+  observer.observe(endElement);
+}
